129f428c1b49e557c6dfbc52199ba14c48800af8
Stan LEE <Cyderpunk2077@gmail.com>
2021-12-23 12:47:19 +0800

    Site updated: 2021-12-23 12:47:18

---
+++ js/background.js
@@ -0,0 +1,8 @@
+<p>保存后在主题的"config"中的"inject"处引入</p>
+```YML
+inject:
+  head:
+  # - <link rel="stylesheet" href="/xxx.css">
+  bottom:
+    # - <script src="xxxx"></script>
+      - <script src="/js/background.js"></script>
