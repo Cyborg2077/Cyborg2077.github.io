fc987b49013c603561ff9e8ff2e5057aa9fe2b5f
Stan LEE <Cyderpunk2077@gmail.com>
2022-04-18 10:34:39 +0800

    Site updated: 2022-04-18 10:34:38

--- js/runtime.js
+++ js/runtime.js
@@ -27,7 +27,7 @@ setInterval(() => {
       time[4] = nol(second);
     }
     if ((Number(time[2])<22) && (Number(time[2])>7)){
-      currentTimeHtml ="<img class='boardsign' src='https://img.shields.io/badge/本站还在营业中-6adea8?style=social&logo=cakephp' title='距离百年老站也就差不到一百年~'><div id='runtime'>" + time[0] + ' YEAR ' + time[1] + ' DAYS ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div>';
+      currentTimeHtml ="<img class='boardsign' src='https://img.shields.io/badge/本站正在营业中-6adea8?style=social&logo=cakephp' title='距离百年老站也就差不到一百年~'><div id='runtime'>" + time[0] + ' YEAR ' + time[1] + ' DAYS ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div>';
     }
     else{
       currentTimeHtml ="<img class='boardsign' src='https://img.shields.io/badge/本站已经打烊了-6adea8?style=social&logo=coffeescript' title='这个点了应该去睡觉啦，熬夜对身体不好哦'><div id='runtime'>" + time[0] + ' YEAR ' + time[1] + ' DAYS ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div>';
